1275f5a8838d563874bf9ee562fc2efa530c02dc
jpalczewski <jpalczewski@gmail.com>
2015-12-09 14:13:47 +0100

    Dzialajace CRU

---
+++ src/pl/edu/pw/elka/bd/lab6/Employee.java
@@ -0,0 +1,121 @@
+package pl.edu.pw.elka.bd.lab6;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+/**
+ * Created by erxyi on 08.12.2015.
+ */
+public class Employee {
+    int EMPLOYEE_ID;
+    String FNAME;
+    String LNAME;
+    String POSITION;
+    java.sql.Date EMPLOYMENT_DATE;
+    int HOUR_RATE;
+
+    Scanner inputScanner;
+    boolean updateMode;
+    Employee(int id, Scanner s) throws ParseException
+    {
+        EMPLOYEE_ID = id;
+        inputScanner = s;
+        updateMode = false;
+
+
+        interactWithUser();
+    }
+    Employee(ResultSet rs, Scanner s) throws SQLException, ParseException
+    {
+        inputScanner = s;
+        updateMode = true;
+
+        EMPLOYEE_ID = rs.getInt("EMPLOYEE_ID");
+        FNAME = rs.getString("FNAME");
+        LNAME = rs.getString("LNAME");
+        POSITION = rs.getString("POSITION");
+        EMPLOYMENT_DATE = rs.getDate("EMPLOYMENT_DATE");
+        HOUR_RATE = rs.getInt("HOUR_RATE");
+
+        System.out.println("Actual data:");
+        System.out.format("%d. %20s %40s %20s %15s %8d\n",
+                rs.getInt("EMPLOYEE_ID"), rs.getString("FNAME"),            rs.getString("LNAME"),
+                rs.getString("POSITION"), rs.getDate("EMPLOYMENT_DATE"),    rs.getInt("HOUR_RATE"));
+
+        System.out.println("If you want to skip something, press enter");
+        interactWithUser();
+
+    }
+    private String readString(String msg, String oldvalue)
+    {
+        String input;
+        System.out.print(msg);
+        input = inputScanner.next();
+            if (!updateMode || !input.isEmpty()) //if(!updateMode || (updateMode && !input.isEmpty()))
+                return input;
+            else
+                return oldvalue;
+
+    }
+    private void interactWithUser() throws ParseException
+    {
+        DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+        String input;
+        java.sql.Date temp;
+        int tempval;
+
+        FNAME = readString("First name:", FNAME);
+        LNAME = readString("Last name:", LNAME);
+        POSITION = readString("Position:", POSITION);
+
+        System.out.print("Employment date(yyyy/MM/dd):");
+        input = inputScanner.next();
+        temp = new java.sql.Date(df.parse(input).getTime());
+        if (!updateMode || !input.isEmpty())
+            EMPLOYMENT_DATE = temp;
+
+        System.out.print("Hour rate(-1 don't update while editing):");
+        tempval = inputScanner.nextInt();
+        if (!updateMode || tempval!=-1)
+            HOUR_RATE = tempval;
+    }
+
+
+    public PreparedStatement makeStatement(Connection conn) throws SQLException
+    {
+        PreparedStatement ps = conn.prepareStatement("insert into EMPLOYEE values (:1, :2, :3, :4, :5, :6, 0)");
+
+        ps.setInt(1, EMPLOYEE_ID);
+        ps.setString(2, FNAME);
+        ps.setString(3, LNAME);
+        ps.setString(4, POSITION);
+        ps.setDate(5, EMPLOYMENT_DATE);
+        ps.setInt(6, HOUR_RATE);
+
+        return ps;
+    }
+
+    public PreparedStatement updateStatement(Connection conn, int version) throws SQLException
+    {
+        PreparedStatement ps = conn.prepareStatement("update EMPLOYEE SET FNAME=:1, LNAME=:2, POSITION=:3, EMPLOYMENT_DATE=:4, HOUR_RATE=:5, VERSION=:6 where EMPLOYEE_ID=:7 and VERSION=:8");
+
+        ps.setString(1, FNAME);
+        ps.setString(2, LNAME);
+        ps.setString(3, POSITION);
+        ps.setDate(4, EMPLOYMENT_DATE);
+        ps.setInt(5, HOUR_RATE);
+        ps.setInt(6, version+1);
+        ps.setInt(7, EMPLOYEE_ID);
+        ps.setInt(8, version);
+
+        return ps;
+    }
+
+
+}
--- src/pl/edu/pw/elka/bd/lab6/WorkshopEmployeeCRUD.java
+++ src/pl/edu/pw/elka/bd/lab6/WorkshopEmployeeCRUD.java
@@ -6,8 +6,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.util.Scanner;
 
+import com.sun.corba.se.impl.orb.PrefixParserAction;
 import oracle.jdbc.pool.OracleDataSource;
 
 
@@ -22,22 +24,31 @@ import oracle.jdbc.pool.OracleDataSource;
 public class WorkshopEmployeeCRUD {
 
 	//const's
-	private static String JDBC_URL = "jdbc:oracle:thin:jpalczew/jpalczew@ora3.elka.pw.edu.pl:1521:ora3inf";
-	
 	
 	private OracleDataSource ods;
 	private Connection conn;
 	private Scanner inputScanner = new Scanner(System.in);
-	
-	
-	
+
+
+    private String getPassword() {
+        System.out.print("User password:");
+        return inputScanner.next();
+    }
+
+    private String generateJDBC_URL(String password) {
+        String prefix = "jdbc:oracle:thin:jpalczew/";
+        String postfix = "@ora3.elka.pw.edu.pl:1521:ora3inf";
+        return prefix + password + postfix;
+    }
+
 	/**
 	 * Initializing DB connection.
 	 * @throws SQLException
 	 */
 	private void init() throws SQLException {
 		ods = new OracleDataSource();
-		ods.setURL(JDBC_URL);
+        String password = getPassword();
+		ods.setURL(generateJDBC_URL(password));
 		conn = ods.getConnection();
 		DatabaseMetaData meta = conn.getMetaData();
 		System.out.println("Successfully connected to DB. JDBC driver version is " + meta.getDriverVersion());
@@ -89,14 +100,76 @@ public class WorkshopEmployeeCRUD {
 	       System.out.println("[" + i + "]:" + rset.getString(2));
 	       i++;
 	    }
-	    
+
+
+
 	    // close the result set, the statement and connect
 	    rset.close();
 	    stmt.close();
 	    
 	}
-	
-	
+
+    private void readAllEmployees() throws SQLException {
+        System.out.println("Reading all employees...");
+
+        // Create a statement & Execute SQL
+        Statement stmt = conn.createStatement();
+        ResultSet rset = stmt.executeQuery("select * FROM EMPLOYEE");
+
+        System.out.println("Query result: ");
+
+        String form = "%d. %20s %40s %20s %15s %8d\n";
+
+        System.out.format("%s %20s %40s %20s %15s %8s\n", "ID", "FIRST NAME", "LAST NAME", "POSITION", "EMP. DATE", "HR_RATE");
+        while (rset.next()) {
+            System.out.format(form,
+                    rset.getInt("EMPLOYEE_ID"), rset.getString("FNAME"),            rset.getString("LNAME"),
+                    rset.getString("POSITION"), rset.getDate("EMPLOYMENT_DATE"),    rset.getInt("HOUR_RATE"));
+
+        }
+
+        // close the result set, the statement and connect
+        rset.close();
+        stmt.close();
+
+    }
+
+    private int getLastID() throws Exception {
+        try {
+            PreparedStatement ps = conn.prepareStatement("select max(EMPLOYEE_ID) from EMPLOYEE");
+            ResultSet rs = ps.executeQuery();
+            if(rs.getFetchSize()==0)
+                return 0;
+            rs.next();
+            int retValue = rs.getInt(1);
+            rs.close();
+            ps.close();
+            return (1+retValue);
+
+        }
+        catch(SQLException se) {
+            throw new Exception("getLastE_ID failed\n"+se.getLocalizedMessage());
+        }
+    }
+
+    private void createNewEmployee() throws Exception {
+        int employeeNr = getLastID();
+        System.out.println("Preparing new employee, nr " + employeeNr);
+        Employee newEmployee = new Employee(employeeNr, inputScanner);
+        PreparedStatement ps = newEmployee.makeStatement(conn);
+
+        try {
+            ps.executeQuery();
+        }
+        catch(SQLException se)
+        {
+            System.out.println("SQLException thrown - possibly someone stolen your EMPLOYEE_ID. Try again.");
+            System.out.println(se.getLocalizedMessage());
+        }
+
+
+    }
+
 	/**
 	 * Prepared statement showcase.
 	 * @throws SQLException
@@ -149,7 +222,51 @@ public class WorkshopEmployeeCRUD {
 		//conn.commit();
 
 	}
-	
+
+    private void updateEmployee() throws SQLException, ParseException {
+        int who, version;
+        ResultSet ret;
+        System.out.print("Enter employee id to continue:");
+        who = inputScanner.nextInt();
+
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM EMPLOYEE WHERE EMPLOYEE_ID=:1");
+        ps.setInt(1, who);
+
+        try {
+            ret = ps.executeQuery();
+            if(!ret.next()) throw new IllegalArgumentException("Employee not found!");
+        }
+        catch(SQLException se)
+        {
+            System.out.println(se.getLocalizedMessage());
+            return;
+        }
+        catch(IllegalArgumentException ile)
+        {
+            System.out.println(ile.getMessage());
+            return;
+        }
+        System.out.println("OK - employee found!");
+        version = ret.getInt("VERSION");
+
+        Employee upEmp = new Employee(ret, inputScanner);
+        try {
+			PreparedStatement ps2 = upEmp.updateStatement(conn, version);
+			int updated = ps2.executeUpdate();
+            if(updated==0) throw new IllegalArgumentException("Someone edited that employee before you - try again.");
+		}
+        catch(SQLException se) {
+            System.out.println("Can't update record:\n" + se.getLocalizedMessage());
+            return;
+        }
+        catch(IllegalArgumentException ile) {
+            System.out.println(ile.getMessage());
+            return;
+        }
+
+        System.out.println("Record updated!");
+    }
+
 	/**
 	 * Shows a menu.
 	 * 
@@ -165,40 +282,52 @@ public class WorkshopEmployeeCRUD {
 							"\t (d) Delete an employee\n"  +
 							"\t (t) Connect an employee with repair\n" +
 							"\t (e) Exit\n");
-		System.out.println("Selected option(c,r,u,d,t):");
+		System.out.println("Selected option(c,r,u,d,t,e):");
 		userResponse = inputScanner.next(".").charAt(0);
-		switch(userResponse)
-		{
-		case 'c':
-			System.out.println("ccc");
-			break;
-		case 'r':
-			System.out.println("rrr");
-			break;
-		case 'u':
-			System.out.println("uuu");
-			break;
-		case 'd':
-			System.out.println("ddd");
-			break;
-		case 't':
-			System.out.println("ttt");
-			break;
-		case 'e':
-			return 0;
-		default:
-			System.out.println("Not recognized");
-			return 0;
-		}
-		
+
+        try {
+            switch(userResponse)
+            {
+            case 'c':
+                createNewEmployee();
+                break;
+            case 'r':
+                readAllEmployees();
+                break;
+            case 'u':
+                updateEmployee();
+                break;
+            case 'd':
+                System.out.println("ddd");
+                break;
+            case 't':
+                System.out.println("ttt");
+                break;
+            case 'e':
+                return 0;
+            default:
+                System.out.println("Not recognized");
+                return 0;
+            }
+        }
+        catch(SQLException se) {
+            System.out.println("SQL Exception thrown!");
+            System.out.println("Exception msg: " + se.getLocalizedMessage());
+            System.out.println("Exiting...");
+            return 1;
+        }
+        catch(Exception e) {
+            System.out.println("General exception thrown!");
+            System.out.println(e.getLocalizedMessage());
+            return 1;
+        }
 		return 1;
 	}		
 	
 	
 	/**
-	 * Run showcase.
-	 * 
-	 * @param args
+	 * Runs menu.
+	 *
 	 */
 	public static void main(String[] args) throws SQLException {
 		System.out.println("WorkshopEmployeeCRUD");
@@ -214,7 +343,7 @@ public class WorkshopEmployeeCRUD {
 		catch(SQLException e)
 		{
 			System.out.println("Error: cannot connect with database!");
-			returnValue = 1;
+			System.exit(1);
 		}
 		
 		
@@ -233,9 +362,7 @@ public class WorkshopEmployeeCRUD {
 		{
 			wec.close();
 		}
-		
-		
-		//return returnValue;
+        System.exit(returnValue);
 	}
 
 }
